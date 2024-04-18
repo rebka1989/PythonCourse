a5c140c3420b5bae25ca6eebd056d6a4820c6cae
rebka1989 <126775425+rebka1989@users.noreply.github.com>
2024-04-18 11:57:47 +0300

    more code

--- language.py
+++ language.py
@@ -1,9 +1,9 @@
 language = input("What is this programming language? ")
-if language == "python": 
-    print("You are right!")
+if language.lower () == "python": 
+    print(f"You are right! It is {language}")
 else:
     print("Nope!")
-    
+
 print("done")
 
 
---
+++ random.py
@@ -0,0 +1,9 @@
+#using random number generators
+
+import random
+
+number = random.random()
+print(number)
+dice = random.randrange(1, 7)
+print(dice)
+
