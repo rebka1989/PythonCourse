9ea6a7032dbd0b4364e927d84cd8e4cf25e35c9d
rebka1989 <126775425+rebka1989@users.noreply.github.com>
2024-04-30 22:39:04 +0300

    updated code

--- day02/random_number_game.py
+++ day02/random_number_game.py
@@ -12,8 +12,10 @@ while True:
         print("The number you guessed is too big, try again.")
     else:
         print("Nice, you're correct!")
+        guess_number += 1 # add 1 guess for the user being correct 
         break # here the game ends, if the user is correct
 
-guess_number += 1
+      guess_number += 1 # add a guess if the user is incorrect
+
 print("The number of guesses it took you is: ", guess_number)
 
