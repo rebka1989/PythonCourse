010153cccc366840454d894dea1bfa1f94b2a26a
rebka1989 <126775425+rebka1989@users.noreply.github.com>
2024-05-07 20:20:00 +0300

    made file with rectangle practice

---
+++ day03/rectangle_in_functions.py
@@ -0,0 +1,11 @@
+def display(area):
+    print(f"The area is {area}")
+
+def main():
+    length = input("Length: ")
+    width = input("Width: ")
+
+    area = int(length) * int(width)
+    display(area)
+
+main()
