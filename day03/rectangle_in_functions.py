60e771864370e72c9ec5e658a7d36821dae1498d
rebka1989 <126775425+rebka1989@users.noreply.github.com>
2024-05-07 20:49:04 +0300

    changes made

--- day03/rectangle_in_functions.py
+++ day03/rectangle_in_functions.py
@@ -1,9 +1,14 @@
-def display(area):
-    print(f"The area is {area}")
+def display(data):
+    print(f"The area is {data}")
+
+def get_input():
+    received_length = input("Length: ")
+    received_width = input("Width: ")
+
+    return (received_length, received_width)
 
 def main():
-    length = input("Length: ")
-    width = input("Width: ")
+    (length, width) = get_input()
 
     area = int(length) * int(width)
     display(area)
